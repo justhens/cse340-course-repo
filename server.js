74281f54d91f1c1b788d85124bf17e4c918b01c0
justhens <justhens@gmail.com>
2026-08-31 16:57:52 -0400

    Rename header.ejs and create separate footer ejs file

--- server.js
+++ server.js
@@ -43,3 +43,7 @@ app.get('/projects', async (req, res) => {
     const title = 'Service Projects';
     res.render('projects', { title });
 });
+app.listen(PORT, () => {
+  console.log(`Server is running at http://127.0.0.1:${PORT}`);
+  console.log(`Environment: ${NODE_ENV}`);
+});
