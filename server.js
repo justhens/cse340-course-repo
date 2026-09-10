277b9ba4c8f686f85bb36b544488c629fb57a1df
justhens <justhens@gmail.com>
2026-09-09 21:59:57 -0400

    W02 Team Activity: Database Entities for Service Projects

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import { testConnection } from './src/models/db.js';
 import { getAllOrganizations } from './src/models/organizations.js';
+import { getAllProjects} from './src/models/projects.js'; 
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -45,7 +46,9 @@ app.get('/organizations', async (req, res) => {
 
 app.get('/projects', async (req, res) => {
     const title = 'Service Projects';
-    res.render('projects', { title });
+    const projects = await getAllProjects();
+
+    res.render('projects', { title, projects });
 });
 app.get('/categories', async (req, res) => {
     const title = 'Service Project Categories';
